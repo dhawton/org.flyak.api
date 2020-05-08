4232c42c2694dca8f4d7408c25616925afb05e57
Daniel Hawton <daniel@hawton.org>
2020-05-07 21:14:58 -0500

    Remove extra / routing for airport controller

--- src/main/java/org/flyak/api/controllers/AirportController.java
+++ src/main/java/org/flyak/api/controllers/AirportController.java
@@ -100,7 +100,7 @@ public class AirportController {
 
     @RequestMapping(
             value = {
-                "", "/"
+                ""
             },
             method = {
                     RequestMethod.POST,
