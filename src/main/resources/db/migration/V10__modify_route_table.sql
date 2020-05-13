7f70b945dab5464855f3c99ce54448babe91697f
Daniel Hawton <daniel@hawton.org>
2020-05-12 20:14:45 -0500

    change migration back.. fuck you travis

--- src/main/resources/db/migration/V10__modify_route_table.sql
+++ src/main/resources/db/migration/V10__modify_route_table.sql
@@ -1,2 +1,2 @@
 ALTER TABLE routes ADD COLUMN arrival_time VARCHAR(5) NOT NULL AFTER departure_time;
-ALTER TABLE routes MODIFY COLUMN days days VARCHAR(128);
+ALTER TABLE routes CHANGE COLUMN days days VARCHAR(128);
