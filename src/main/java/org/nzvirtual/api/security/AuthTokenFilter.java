51be572085edf469f92fd44e2c33a215fe6ad85d
Daniel Hawton <daniel@hawton.org>
2020-05-15 08:39:33 -0500

    Add basic auth to authfilter

--- src/main/java/org/nzvirtual/api/security/AuthTokenFilter.java
+++ src/main/java/org/nzvirtual/api/security/AuthTokenFilter.java
@@ -3,7 +3,9 @@ package org.nzvirtual.api.security;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -15,12 +17,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Base64;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
     @Autowired
     private JwtUtils jwtUtils;
     @Autowired
     private UserDetailsServiceImpl userDetailService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
     private static final Logger log = LoggerFactory.getLogger(AuthTokenFilter.class);
 
     @Override
@@ -38,19 +43,44 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
+
+            String basic = parseBasic(request);
+            if (basic != null) {
+                String authString = new String(Base64.getUrlDecoder().decode(basic));
+                String[] parts = authString.split(":", 2);
+                String username = parts[0];
+                String password = parts[1];
+
+                Authentication authentication = authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(username, password)
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         } catch (Exception e) {
-            logger.error("Cannot set user authentication: " + e.getMessage());
-            logger.error(e.getStackTrace());
+            logger.error("Cannot set user authentication: " + e.getMessage(), e);
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private String parseBasic(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Basic ")) {
+            log.info("Header: " + headerAuth);
+            log.info("Header SubString: " + headerAuth.substring(6));
+            return headerAuth.substring(6);
+        }
+
+        return null;
     }
 
     private String parseJwt(HttpServletRequest request) {
         String headerAuth = request.getHeader("Authorization");
 
         if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
-            return headerAuth.substring(7, headerAuth.length());
+            return headerAuth.substring(7);
         }
 
         return null;
