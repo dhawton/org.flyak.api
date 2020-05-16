3b037ad6e6706b2d32f40069b480e873d0a06378
Daniel Hawton <daniel@hawton.org>
2020-05-15 21:49:42 -0500

    Change versioning

--- src/main/java/org/nzvirtual/api/controllers/ServerController.java
+++ src/main/java/org/nzvirtual/api/controllers/ServerController.java
@@ -2,6 +2,7 @@ package org.nzvirtual.api.controllers;
 
 import org.nzvirtual.api.ApiApplication;
 import org.nzvirtual.api.dto.ServerInfoResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/server")
 public class ServerController {
+    @Value("${app.version}")
+    private String appversion;
+
     @GetMapping()
     public ResponseEntity<ServerInfoResponse> getServerInfo() {
-        return new ResponseEntity<>(new ServerInfoResponse(ApiApplication.getVersion()), HttpStatus.OK);
+        return new ResponseEntity<>(new ServerInfoResponse(String.format("%s-%s", appversion, ApiApplication.getVersion())), HttpStatus.OK);
     }
 
     @GetMapping("/ping")
