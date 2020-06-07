c9b790741f3209cb483c8ce6468c15ff19fe9d0b
Daniel Hawton <daniel@hawton.org>
2020-06-07 16:39:35 -0500

    migration add foreign key

--- src/main/resources/db/migration/V16__logbook_details.sql
+++ src/main/resources/db/migration/V16__logbook_details.sql
@@ -1,2 +1,3 @@
 ALTER TABLE logbook_details ADD COLUMN created_at DATETIME;
 ALTER TABLE logbook_details MODIFY COLUMN entry TEXT;
+ALTER TABLE logbook_details ADD FOREIGN KEY (logbook_id) REFERENCES logbook(id) ON DELETE CASCADE;
